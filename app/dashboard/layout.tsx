35fb2a940d5de47c98416e6514c3e8eda59e59f9
Rachit <rachit.gupta4@gmail.com>
2024-05-25 23:33:00 +0200

    mobile responsive

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -12,11 +12,19 @@ export default function Layout({
   return (
     <div className="flex flex-col">
       {children}
-      <div className="flex w-full justify-center">
-        <div className="p-4 grow">
+      <div className="flex w-full justify-center max-lg:hidden">
+        <div className="p-4 grow lg:col-span-full ">
           {currentPrice}
         </div>
-        <div className="p-4 grow">
+        <div className="p-4 grow lg:col-span-full">
+          {priceGraph}
+        </div>
+      </div>
+      <div className=" w-full justify-center lg:hidden">
+        <div className="text-light-1 p-4 grow lg:col-span-full ">
+         rwrwwq {currentPrice} wrwrewer
+        </div>
+        <div className="p-4 grow lg:col-span-full">
           {priceGraph}
         </div>
       </div>
